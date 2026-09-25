e5fd98bb83c393a3dc7e49454fbd0ad7d83a94a4
Manpreet Kour <mpk100805@gmail.com>
2026-09-26 00:38:00 +0530

    Time: 7 ms (63.55%), Space: 19.3 MB (58.86%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
@@ -1,17 +1,23 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        if len(s)<k:
+        if len(s) < k:
             return 0
-        freq={}
-        for ch in s:
-            freq[ch]=1+freq.get(ch,0)
-        for ch in freq:
-            if freq[ch]<k:
-                parts= s.split(ch)
-                ans=0
-                for part in parts:
-                    ans=max(ans,self.longestSubstring(part,k))
-                return ans
-        return len(s)
 
+        count = {}
+
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        for ch in count:
+            if count[ch] < k:
+                left = self.longestSubstring(s.split(ch)[0], k)
+                right = 0
+
+                parts = s.split(ch)
+                for part in parts[1:]:
+                    right = max(right, self.longestSubstring(part, k))
+
+                return max(left, right)
+
+        return len(s)
         
